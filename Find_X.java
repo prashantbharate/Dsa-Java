1fd3c72c849163bd9f7aeb31d82d681c927160b9
prashantbharate <prshntbharate77@gmail.com>
2023-04-04 17:00:30 +0530

    Add files via upload

---
+++ Find_X.java
@@ -0,0 +1,20 @@
+//Find X
+
+//Enter code here
+import java.util.*;
+
+class Find_X
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        
+        int x=sc.nextInt();
+        
+             x=x*3;
+             x= x+10;
+           
+        
+        System.out.println(x);
+    }
+}
