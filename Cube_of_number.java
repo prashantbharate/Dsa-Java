02d31dae391aea4317ef9dba35433d29c030c142
prashantbharate <prshntbharate77@gmail.com>
2023-04-04 16:29:42 +0530

    Add files via upload
    cube of a number

---
+++ Cube_of_number.java
@@ -0,0 +1,17 @@
+//Enter code here
+
+import java.util.*;
+class Cube_of_number
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        
+        int total=0;
+        
+           int num=sc.nextInt();
+           total=num*num*num;
+     
+       System.out.println(total);
+    }
+}
